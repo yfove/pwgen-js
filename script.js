6ce7405a11d7236d8a38c0ede2e3263eef37968e
curtis chat <curtis.ch.chat@gmail.com>
2019-12-20 19:21:30 -0700

    loops over length and calls generate function

--- script.js
+++ script.js
@@ -16,15 +16,59 @@ const randomFunc = {
   symbol: getRandomSymbol
 };
 
+// Generate event listen
 generateEl.addEventListener('click', () => {
   const length = +lengthEl.value; //when it is clicked we need to get the value
   const hasUpper = uppercaseEl.checked;
   const hasLower = lowercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
-  // console.log(hasLower, hasUpper, hasNumber, hasSymbol, length );
-  function generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length );
-})
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Generate password function
+  function generatePassword(lower, upper, number, symbol, length ) {
+  // init pw var
+  // filter out unchecked types
+  // loop over the length call generator function for each type
+  // add final pw to the pw var and return
+
+  let generatePassword = '';
+  const typesCount = lower + upper + number + symbol; 
+  // console.log('typesCount: ', typesCount); //count the number of checked items
+
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+  (
+    item => Object.values(item)[0]
+  );
+
+  // console.log('typesArr: ', typesArr);
+
+  if(typesCount === 0) {
+    return '';
+  }
+
+  //loop over lengths and call generator function
+  for(let i=0; i < length; i+= typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      // console.log('funcName: ', funcName); //lower upper number symbol
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+
+
+}
+
+
 
 
 function getRandomLower() {
@@ -43,5 +87,3 @@ function getRandomSymbol() {
   const symbols = '~!@#$%^&*()-+=?'
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
-console.log(getRandomLower());
